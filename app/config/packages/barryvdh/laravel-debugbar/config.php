1483498b19aa1113f452155755c5c54fac8a23a9
Mohammad Sadeghi <sadeghi85@hotmail.com>
2014-08-30 16:57:37 +0430

    .

--- app/config/packages/barryvdh/laravel-debugbar/config.php
+++ app/config/packages/barryvdh/laravel-debugbar/config.php
@@ -88,12 +88,12 @@ return array(
         'db'              => true,  // Show database (PDO) queries and bindings
         'views'           => true,  // Views with their data
         'route'           => true,  // Current route information
-        'laravel'         => false, // Laravel version and environment
-        'events'          => false, // All events fired
+        'laravel'         => true, // Laravel version and environment
+        'events'          => true, // All events fired
         'default_request' => false, // Regular or special Symfony request logger
         'symfony_request' => true,  // Only one can be enabled..
-        'mail'            => true,  // Catch mail messages
-        'logs'            => false, // Add the latest log messages
+        'mail'            => false,  // Catch mail messages
+        'logs'            => true, // Add the latest log messages
         'files'           => false, // Show the included files
         'config'          => false, // Display config settings
         'auth'            => false, // Display Laravel authentication status
